70b507b11e2e3011e62de1513f689628952cd635
Hiroyuki Ogasawara <hiroog@flatlib.jp>
2024-08-18 00:02:54 +0900

    古いファイルの削除

--- ControllerClient/RemoteControllerClient.bat
+++ ControllerClient/RemoteControllerClient.bat
@@ -2,4 +2,8 @@
 
 cd /D "%~dp0"
 
+if exist win32\x64\Release\remote_client2022.exe (
+copy win32\x64\Release\remote_client2022.exe remote_client.exe
+)
+
 start remote_client.exe -cconfig.txt -tpad_table.txt
